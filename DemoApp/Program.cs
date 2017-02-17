5753ea168e0c34ba8dde5caf74ae2214913ddfaf
Denis Zavershinskiy <zaverden@gmail.com>
2017-02-17 19:43:03 +0700

    Add project files.

---
+++ DemoApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using DemoClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asdasdasd");
+            Console.ReadKey();
+            new Class1();
+        }
+    }
+}
